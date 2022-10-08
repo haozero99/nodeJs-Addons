926aacc8bcd423ffe866b712451cd3e6e29b5b64
zhumaolin@zuler.io <zhumaolin@zuler.io>
2022-10-08 10:21:06 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# nodeJs-Addons
+nodejs c++ 扩展
+
+1. npm i -g  node-gyp
+
+2. create binding.gyp file
+
+3. create calculate.cc file
+
+4. create app.js file
+
+5. compile c++ file: node-gyp configure build
+
+6. run app.js
+
+node app.js
---
+++ app.js
@@ -0,0 +1,23 @@
+const calculate = require('./build/Release/calculate');
+  
+// Javascript add function.
+function calc() {
+  
+    // Two variables.
+    let i, x = 3.141526, y = 2.718;
+    for (i = 0; i < 1000000000; i++) {
+        x += y;
+  
+    }
+    let total = x;
+    return total;
+}
+  
+console.time('c++');
+calculate.calc();
+console.timeEnd('c++');
+  
+console.time('js');
+calc();
+  
+console.timeEnd('js');
